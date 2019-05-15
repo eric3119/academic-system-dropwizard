ad76025a97a47410b7616ecda17f5e31fae61be8
eric.santos <eric.santos@tce.al.gov.br>
2019-05-15 17:50:57 -0300

    require args Student

--- src/main/java/br/ufal/ic/br/ufal/ic/model/Student.java
+++ src/main/java/br/ufal/ic/br/ufal/ic/model/Student.java
@@ -1,6 +1,12 @@
 package br.ufal.ic.br.ufal.ic.model;
 
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
 public class Student {
+    @NonNull
     private String name;
+    @NonNull
     private String code;
 }
