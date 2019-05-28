1e2e68e9e0b7583444c2dfd549bee19c6f40b33b
eric3119 <esc2@ic.ufal.br>
2019-05-28 09:27:43 -0300

    add resources

---
+++ src/main/java/br/ufal/ic/resources/AcademicSystemResources.java
@@ -0,0 +1,33 @@
+package br.ufal.ic.resources;
+
+import br.ufal.ic.api.Saying;
+import com.codahale.metrics.annotation.Timed;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.MediaType;
+import java.util.concurrent.atomic.AtomicLong;
+import java.util.Optional;
+
+@Path("/academic")
+@Produces(MediaType.APPLICATION_JSON)
+public class AcademicSystemResources {
+    private final String template;
+    private final String defaultName;
+    private final AtomicLong counter;
+
+    public AcademicSystemResources(String template, String defaultName) {
+        this.template = template;
+        this.defaultName = defaultName;
+        this.counter = new AtomicLong();
+    }
+
+    @GET
+    @Timed
+    public Saying sayHello(@QueryParam("name") Optional<String> name){
+        final String value = String.format(template, name.orElse(defaultName));
+        return new Saying(counter.incrementAndGet(), value);
+    }
+}
