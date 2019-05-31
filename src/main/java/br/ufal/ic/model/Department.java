decb46dcae08e834108aa883d5af325e9d0bd6c4
eric3119 <esc2@ic.ufal.br>
2019-05-31 10:22:04 -0300

    id annotation department, manytoone student department

--- src/main/java/br/ufal/ic/model/Department.java
+++ src/main/java/br/ufal/ic/model/Department.java
@@ -1,6 +1,15 @@
 package br.ufal.ic.model;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 class Department {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
     private String name;
     private Secretary grad_secr;
     private Secretary pos_grad_secr;
--- src/main/java/br/ufal/ic/model/Student.java
+++ src/main/java/br/ufal/ic/model/Student.java
@@ -3,11 +3,17 @@ package br.ufal.ic.model;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 
+import javax.persistence.ManyToOne;
+
 @RequiredArgsConstructor
 public class Student {
+
     @NonNull private String name;
     @NonNull private String code;
+
+    @ManyToOne
     private Department department;
+
     private Secretary secretary;
     private boolean graduating;
     private Integer credits;
